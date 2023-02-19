a2233dab3422d06535560931a2622b0207e10a9a
Shrenik <shrenikparam1234@gmail.com>
2023-02-19 20:57:54 +0530

    Initial commit

---
+++ Procurement/scripts/custom/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#deployed").click(() => {
+    window.location.href = "./deployed.html"
+  })
+  $("#manager").click(() => {
+    window.location.href = "./manager.html"
+  })
+  $("#bids").click(() => {
+    window.location.href = "./bids.html"
+  })
+})
