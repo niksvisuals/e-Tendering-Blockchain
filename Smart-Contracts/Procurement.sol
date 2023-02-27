ab6898b72227193ef97c93ad2b9748ca1d68899f
Shrenik <shrenikparam1234@gmail.com>
2023-02-27 22:08:34 +0530

    Breaking changes, fix major bugs

--- Smart-Contracts/Procurement.sol
+++ Smart-Contracts/Procurement.sol
@@ -3,20 +3,34 @@
    contract.
 */
 pragma solidity ^0.4.17;
+// pragma experimental ABIEncoderV2;
 
 contract tenderFactory {
     address[] public deployedTenders;
-    // deployedTenders stores the addresses of the deployed tenders
-    function createTender(string description) public {
-      address newTender = new Tender(description, msg.sender);
-      deployedTenders.push(newTender);
+
+    address private manager;
+
+    // stores the addresses of the deployed tenders
+
+    constructor() public {
+        manager = msg.sender;
     }
+
+    function createTender(string description) public {
+        require(
+            msg.sender == manager,
+            "You are not authorised to create tender."
+        );
+        address newTender = new Tender(description, msg.sender);
+        deployedTenders.push(newTender);
+    }
+
     /* createTender is used to deploy a new instance of the 'tender' contract - 
        it accepts the requirements of the tender as argument and deploys an
        instance with the msg.sender as manager
-    */  
+    */
 
-    function getDeployedTenders() public view returns(address[]){
+    function getDeployedTenders() public view returns (address[]) {
         return deployedTenders;
     }
     /* getDeployedTenders is a function that returns the array of deployed tenders' 
@@ -24,17 +38,17 @@ contract tenderFactory {
     */
 }
 
-contract Tender{
-    bool public complete;
+contract Tender {
+    bool private complete;
     string public data;
-    address public manager;
+    address private manager;
     /* manager => address of the manager/creator of the tender
        data => requirements of the tender 
        complete => status of whether the tender has BEEN AWARDED
     */
     struct Bid {
-        address  bidder;
-        uint bidAmount;
+        address bidder;
+        uint256 bidAmount;
         string bid;
     }
     /* struct Bid is a type to hold the details of a bid made - containing
@@ -42,76 +56,80 @@ contract Tender{
        proposal of the bidder (bid).
     */
     struct hiddenBid {
-        uint ID;
-        uint bidAmount;
+        uint256 ID;
+        uint256 bidAmount;
         string bid;
     }
     /* struct hiddenBid contains the bidAmount and bid members, but does not contain the 
        address of the bidder, to ensure that the awarding of a tender is an unbiased process
     */
     Bid[] private bids;
-    hiddenBid[] public hiddenBids;
+    // hiddenBid[] public hiddenBids;
     Bid public winner;
-    uint public winIndex;
+    uint256 public winIndex;
     /* 1.bids consists of all the bids and is made private to ensure no bias.
        2.hiddenBids is the array of structures that contain bids without their addresses, for 
          the purpose of the govt choosing a bid
        3.winner consists of the address of the winning bidder and winIndex contains the index
     */
-    modifier restricted()
-    {
-        require(msg.sender==manager);
+    modifier restricted() {
+        require(msg.sender == manager, "You are not authorised, only manager can access this.");
         _;
     }
+
     // This modifier is used to check if the sender of the function call is the manager.
-    constructor(string description, address creator) public
-    {
+    constructor(string description, address creator) public {
         manager = creator;
         data = description;
     }
+
     // Constructor function of the tender contract.
 
-    function getBidSummary(uint index) public view returns (address, uint, string) {
-        return (
-            bids[index].bidder,
-            bids[index].bidAmount,
-            bids[index].bid
-        );
+    function getBidSummary(uint256 index)
+        public
+        view
+        returns (
+            address,
+            uint256,
+            string
+        )
+    {
+        return (bids[index].bidder, bids[index].bidAmount, bids[index].bid);
     }
+
     /* This function returns the details of the bidder, bidAmount and bid(proposal)
        only after the tender has been awarded, to ensure transparency in the system.
     */
-    
 
-    function makeBid(uint bidAmount, string desc) public
-    {  require(!(msg.sender==manager));
-       require(!complete);
+    function makeBid(uint256 bidAmount, string desc) public {
+        require(!(msg.sender == manager), "Manager are not allowed to bid.");
+        require(!complete, "Tender results have been declared");
         Bid memory newBid = Bid({
-            bidder : msg.sender,
-            bidAmount : bidAmount,
-            bid : desc
+            bidder: msg.sender,
+            bidAmount: bidAmount,
+            bid: desc
         });
         bids.push(newBid);
-        hiddenBid memory newhiddenBid = hiddenBid({
-            ID : bids.length,
-            bidAmount : bidAmount,
-            bid : desc
+        /*     hiddenBid memory newhiddenBid = hiddenBid({
+            ID: bids.length,
+            bidAmount: bidAmount,
+            bid: desc
         });
-        hiddenBids.push(newhiddenBid);
+        hiddenBids.push(newhiddenBid); */
     }
 
     /* This function is used to let a bidder make a bid, it creates a temporary instance
        of Bid and hiddenBid and initialised them and pushes them into the respective arrays.
     */
-    
-    function finalizeBid(uint index) public  {
-        require(!complete);
-        require(msg.sender==manager);
+
+    function finalizeBid(uint256 index) public {
+        require(!complete,  "Tender results have been declared");
+        require(msg.sender == manager, "Only managers can finalise bids.");
         winner = bids[index];
         winIndex = index;
         // winner.bidder.transfer(winner.bidAmount);
         msg.sender.transfer(address(this).balance);
-        complete = true;   
+        complete = true;
     }
 
     /* finalizeBid is used to award the bid by passing an argument of the index of the bid,
@@ -120,27 +138,32 @@ contract Tender{
        manager of the tender.
     */
 
-    function getSummary() public view returns (address, string, uint) {
-        return(manager,
-        data,
-        bids.length);
+    function getSummary()
+        public
+        view
+        returns (
+            address,
+            string,
+            uint256
+        )
+    {
+        return (manager, data, bids.length);
     }
-    function getBidCount() public view returns (uint) {
+
+    function getBidCount() public view returns (uint256) {
         return bids.length;
     }
-    
-    function verify() public view  {
-      require(msg.sender==manager);
-    }
+
+    /*   function verify() public view {
+        require(msg.sender == manager);
+        return false;
+    } */
 
     function verifyManager() public view returns (bool) {
-        return(manager==msg.sender);
+        return (manager == msg.sender);
     }
-    
-    function status() public view returns(bool)
-    {
+
+    function status() public view returns (bool) {
         return complete;
     }
-
-
 }
